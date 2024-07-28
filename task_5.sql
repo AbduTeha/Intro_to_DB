8170f5ff37e7885039f2823c8de7fc723b861f5b
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-07-28 07:22:30 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,14 @@
--- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- task_5.sql
+
+INSERT INTO customer (
+  customer_id,
+  customer_name,
+  email,
+  address
+)
+VALUES (
+  1,
+  'Cole Baidoo',
+  'cbaidoo@sandtech.com',
+  '123 Happiness Ave.'
+);
